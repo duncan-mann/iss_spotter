665113c0bcb2720dc349d27c6fb75d7c53865608
Duncan Mann <duncan.s.mann@gmail.com>
2019-09-26 23:49:46 +0000

    Deleted unnecessary comments

--- index.js
+++ index.js
@@ -17,30 +17,3 @@ nextISSTimesForMyLocation((error, passTimes) => {
     printPassTimes(passTimes);
   }
 });
-
-//   fetchMyIP((error, ip) => {
-//     if (error) {
-//       console.log("Error: IP not found!" , error);
-//       return;
-//     }
-//     console.log('It worked! Returned IP:' , ip);
-
-//     fetchCoordsByIP(ip, (error, results) => {
-//       if (error) {
-//         console.log("Error: Coordinates not found:", error);
-//         return;
-//       } else {
-//         console.log('Coordinates fetched!', results);
-
-//         fetchISSFlyOverTimes(results, (error, results) => {
-//           if (error) {
-//             console.log('Error: Flyover times not found:', error);
-//             return;
-//           } else {
-//             console.log('Flyover times found! :', results);
-//           }
-//         });
-//       }
-//     });
-//   });
-// });
--- iss.js
+++ iss.js
@@ -1,12 +1,3 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
-
 const request = require('request');
 
 const fetchMyIP = function(callback) {
@@ -46,16 +37,6 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-/**
- * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
- * Input:
- *   - An object with keys `latitude` and `longitude`
- *   - A callback (to pass back an error or the array of resulting data)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The fly over times as an array of objects (null if error). Example:
- *     [ { risetime: 134564234, duration: 600 }, ... ]
- */
 const fetchISSFlyOverTimes = function(coords, callback) {
   
   request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
